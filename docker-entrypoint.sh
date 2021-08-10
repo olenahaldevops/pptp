e3eb4144aff90b684b91331a7c639383084488f3
olenahaldevops <88713848+olenahaldevops@users.noreply.github.com>
2021-08-10 13:17:19 +0300

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cat > /etc/ppp/peers/${TUNNEL} <<_EOF_
+pty "pptp ${SERVER} --nolaunchpppd"
+name "${USERNAME}"
+password "${PASSWORD}"
+remotename PPTP
+require-mppe-128
+file /etc/ppp/options.pptp
+ipparam "${TUNNEL}"
+_EOF_
+
+cat > /etc/ppp/ip-up <<"_EOF_"
+#!/bin/sh
+ip route add 0.0.0.0/1 dev $1
+ip route add 128.0.0.0/1 dev $1
+ip route add 172.10.10.0/24 dev $1
+_EOF_
+
+cat > /etc/ppp/ip-down <<"_EOF_"
+#!/bin/sh
+ip route del 0.0.0.0/1 dev $1
+ip route del 128.0.0.0/1 dev $1
+ip route del 172.10.10.0/24 dev $1
+_EOF_
+
+exec pon ${TUNNEL} debug dump logfd 2 nodetach persist "$@"
